0c2a58385189886308ebe54d1f30439a27806f4a
daradona10 <daradona10@gmail.com>
2020-01-03 12:47:57 +0000

    updating secret key and also the migration files etc

--- my_project/settings.py
+++ my_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 from os import path
 if path.exists("env.py"):
     import env
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'b2$eted7y=*90@d3fdf*cy#x2swn_%1+m$+8tm-&=n^u_!qqoe')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,12 +78,14 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
+
+DATABASES = {'default': dj_database_url.parse("postgres://oueuhczfrbkyzg:d0cd9686b39127cba39c8bbe7d98d4f6f18b2c58eb8439dad950c4e2de3b2453@ec2-46-137-173-221.eu-west-1.compute.amazonaws.com:5432/dd03dob7suig1")}
 
 
 # Password validation
